cfc95e53b5e84ac5238fd369c37fc1cf59a24567
DanielQuesada97 <54914396+DanielQuesada97@users.noreply.github.com>
2019-11-06 13:33:13 -0600

    Update Eje1.cpp

--- LABORATORIOS/Laboratorio_5/Eje1.cpp
+++ LABORATORIOS/Laboratorio_5/Eje1.cpp
@@ -6,14 +6,17 @@ struct nodo{
     nodo *izq;
     nodo *der;
 };
-
+///////////////////////////////////////////////////////////////////////////////
 //PROTOTIPOS
 nodo *crearNodo(int);
 nodo *arbol = NULL;
+int numNodos = 0; // Numero de nodos
 void insertarNodo(nodo *&, int);
 void mostrarArbol(nodo *, int);
 bool busquedaArbol(nodo *, int);
-
+int numeroNiveles(nodo *);
+int sumaArbol(nodo *);
+///////////////////////////////////////////////////////////////////////////////
 int main(){
     int dato, opt, contador = 0;
     char siNo;
@@ -24,13 +27,15 @@ int main(){
         cout<<"1. Insertar un Nuevo Nodo"       <<endl;
         cout<<"2. Mostrar Arbol"                <<endl;
         cout<<"3. Buscar nodo"                  <<endl;
-        cout<<"4. Salir"                  <<endl<<endl;
+        cout<<"4. Cantidad de Niveles"          <<endl;
+        cout<<"5. Suma de los Nodos"            <<endl;
+        cout<<"6. Salir"                  <<endl<<endl;
         cout<<"Elija una opcion para continuar: ";
         cin>>opt;
         
         //OPCIONES DEL MENU
         switch(opt){
-            case 1:
+            case 1: //Insertar
                 cout<<"Digite un numero: ";
                 cin>>dato;
                 insertarNodo(arbol, dato);                
@@ -38,6 +43,8 @@ int main(){
             case 2: 
                 cout<<"Mostrando Arbol completo: "<<endl;
                 mostrarArbol(arbol, contador);
+                cout<<"El numero de nodos es: "<<numNodos<<endl;
+                numNodos = 0;
                 break;
             case 3:
                 cout<<"Digite elemento a buscar: ";
@@ -49,13 +56,18 @@ int main(){
                     cout<<"El elemento no fue encontrado"<<endl;
                 }
                 break;
+            case 4: 
+                cout<<"El numero de niveles es: "<<numeroNiveles(arbol)<<endl;
+                break;
+            case 5: 
+                cout<<"La suma total de los nodos es: "<<sumaArbol(arbol);
+                break;      
         }    
-    }while(opt != 4);
+    }while(opt != 6);
     
     return 0;
 }
-
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //FUNCION PARA CREAR UN NUEVO NODO
 nodo *crearNodo(int n){
     nodo *nuevo_nodo = new nodo();
@@ -66,7 +78,7 @@ nodo *crearNodo(int n){
     
     return nuevo_nodo;
 }
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //INSERTAR NODOS
 void insertarNodo(nodo *&arbol, int n){
     if(arbol == NULL){ //Si el arbol esta vacio
@@ -83,7 +95,7 @@ void insertarNodo(nodo *&arbol, int n){
         }
     }
 } 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //MOSTRAR ARBOL
 void mostrarArbol(nodo *arbol, int cont){
     if(arbol == NULL){//Si el arbol esta vacio
@@ -94,11 +106,12 @@ void mostrarArbol(nodo *arbol, int cont){
         for(int i=0; i<cont; i++){
             cout<<"  ";
         }
+        numNodos++;
         cout<<arbol->dato<<endl;
         mostrarArbol(arbol->izq, cont+1);
     }
 }
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //BUSQUEDA DE UN NODO
 bool busquedaArbol(nodo *arbol, int n){
     if(arbol == NULL){ //Si arbol esta vacio
@@ -114,3 +127,31 @@ bool busquedaArbol(nodo *arbol, int n){
         return busquedaArbol(arbol->der, n);
     }
 }
+///////////////////////////////////////////////////////////////////////////////
+//NUMERO DE NODOS
+int numeroNiveles(nodo *arbol){
+    int AltIzq, AltDer;
+    if (arbol == NULL){
+        return -1;
+    }
+    else{
+        AltIzq = numeroNiveles(arbol->izq);
+        AltDer = numeroNiveles(arbol->der);
+    if (AltIzq > AltDer)
+        return AltIzq + 1;
+    else
+        return AltDer + 1;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+//SUMA DE ARBOL
+int sumaArbol(nodo *arbol){
+    int dato;
+    if(arbol == NULL){
+        return 0;
+    }
+    else{
+        return arbol->dato + sumaArbol(arbol->izq) + sumaArbol(arbol->der);
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
